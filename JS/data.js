053e809c26762a188cb145b6ec067e7d470a3ee6
lucabenaventew <cacachuuuluca@gmail.com>
2022-06-22 19:01:36 -0500

    Update js file

--- JS/data.js
+++ JS/data.js
@@ -1,40 +1,40 @@
 const cardData = [
-    {
-      img: 'https://randomuser.me/api/portraits/men/1.jpg',
-      nombre: 'Elon Musk',
-      cargo: 'CEO of SpaceX, Tesla, The boring company',
-      description: 'Elon Musk, el hombre más rico del mundo se presentará en nuestro evento.'
-    },
-    {
-      img: 'https://randomuser.me/api/portraits/men/2.jpg',
-      nombre: 'Sir Richard Branson',
-      cargo: 'CEO and Founder of Virgin Galactic',
-      description: 'El señor Branson fue la primera persona mayor de 80 años en visitar el espacio.'
-    },
-    {
-      img: 'https://randomuser.me/api/portraits/men/4.jpg',
-      nombre: 'Jeff Bezzos',
-      cargo: 'CEO and founder of Blue Origin and Amazon',
-      description: 'Mr. Bezzos estará con nostros contandonos su experiencia como CEO de la empresa más gigantesca del mundo.'
-    },
-    {
-      img: 'https://randomuser.me/api/portraits/women/3.jpg',
-      nombre: 'Ana Espinoa',
-      cargo: 'CoFounder of Women in Space',
-      description: 'Women in space es un programa que se encargará de llevar una nave de mujeres a la Luna.'
-    },
-    {
-      img: 'https://randomuser.me/api/portraits/women/7.jpg',
-      nombre: 'Pamela Gutierrez',
-      cargo: 'CEO of SpaceLATAM',
-      description: 'Space LATAM es la primera empresa latinoamericana enfocada al ámbito espacial.'
-    },
-    {
-      img: 'https://randomuser.me/api/portraits/women/6.jpg',
-      nombre: 'Yochai Benkler',
-      cargo: 'Profesora en la Universidad de Harvard',
-      description: 'La profesora Benkler nos contará acerca de su investigacion sobre agujeros negros.'
-    },
-  ];
-  
-  export default cardData;
+  {
+    img: 'https://randomuser.me/api/portraits/men/1.jpg',
+    nombre: 'Elon Musk',
+    cargo: 'CEO of SpaceX, Tesla, The boring company',
+    description: 'Elon Musk, el hombre más rico del mundo se presentará en nuestro evento.'
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/men/2.jpg',
+    nombre: 'Sir Richard Branson',
+    cargo: 'CEO and Founder of Virgin Galactic',
+    description: 'El señor Branson fue la primera persona mayor de 80 años en visitar el espacio.'
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/men/4.jpg',
+    nombre: 'Jeff Bezzos',
+    cargo: 'CEO and founder of Blue Origin and Amazon',
+    description: 'Mr. Bezzos estará con nostros contandonos su experiencia como CEO de la empresa más gigantesca del mundo.'
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/3.jpg',
+    nombre: 'Ana Espinoa',
+    cargo: 'CoFounder of Women in Space',
+    description: 'Women in space es un programa que se encargará de llevar una nave de mujeres a la Luna.'
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/7.jpg',
+    nombre: 'Pamela Gutierrez',
+    cargo: 'CEO of SpaceLATAM',
+    description: 'Space LATAM es la primera empresa latinoamericana enfocada al ámbito espacial.'
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/6.jpg',
+    nombre: 'Yochai Benkler',
+    cargo: 'Profesora en la Universidad de Harvard',
+    description: 'La profesora Benkler nos contará acerca de su investigacion sobre agujeros negros.'
+  },
+];
+
+export default cardData;
