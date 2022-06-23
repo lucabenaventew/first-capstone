98e722ba1b8bb1ea19d2ed1120813c5cd1b329bc
lucabenaventew <cacachuuuluca@gmail.com>
2022-06-22 19:01:04 -0500

    Update js file

--- JS/dinamic.js
+++ JS/dinamic.js
@@ -11,28 +11,28 @@ const featuredSCards = document.createElement('div');
 featuredSCards.classList.add('featured_speakers_cards');
 
 for (let i = 0; i < cardData.length; i += 1) {
-    const Scard = document.createElement('div');
-    Scard.classList.add('featured_speakers_card');
-    const img = document.createElement('img');
-    img.src = cardData[i].img;
-    const div = document.createElement('div');
-    div.classList.add('card_text');
-    const h1 = document.createElement('h1');
-    h1.textContent = cardData[i].nombre;
-    const fp = document.createElement('p');
-    fp.textContent = cardData[i].cargo;
-    const sp = document.createElement('p');
-    sp.textContent = cardData[i].description;
+  const Scard = document.createElement('div');
+  Scard.classList.add('featured_speakers_card');
+  const img = document.createElement('img');
+  img.src = cardData[i].img;
+  const div = document.createElement('div');
+  div.classList.add('card_text');
+  const h1 = document.createElement('h1');
+  h1.textContent = cardData[i].nombre;
+  const fp = document.createElement('p');
+  fp.textContent = cardData[i].cargo;
+  const sp = document.createElement('p');
+  sp.textContent = cardData[i].description;
 
-    featuredSCards.appendChild(Scard);
-    Scard.appendChild(img);
-    Scard.appendChild(div);
-    div.appendChild(h1);
-    div.appendChild(fp);
-    div.appendChild(sp);
+  featuredSCards.appendChild(Scard);
+  Scard.appendChild(img);
+  Scard.appendChild(div);
+  div.appendChild(h1);
+  div.appendChild(fp);
+  div.appendChild(sp);
 }
 
 section.appendChild(container);
 container.appendChild(containerH2);
 container.appendChild(firstSpan);
-container.appendChild(featuredSCards);
+container.appendChild(featuredSCards);  
